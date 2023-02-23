a57a9e5c4841a20d9ba05cf6f9084b9acfc8ea6a
Anamika Rawat <anamikarawart@gmail.com>
2023-02-23 22:13:21 +0530

    add

--- client/script.js
+++ client/script.js
@@ -92,7 +92,8 @@ const handleSubmit = async (e) => {
 
   //**********************connecting client and server****************************
   //fetch data from server -> bot's response
- const response = await fetch('http://localhost:5000', {
+ const response = await fetch('https://chat-bot-backend.onrender.com', {
+  //http://localhost:5000 is the url of the server
     method: "POST",
     headers: {
       "Content-Type": "application/json",
